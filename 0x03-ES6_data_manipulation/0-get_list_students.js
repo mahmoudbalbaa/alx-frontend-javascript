60e8cd2cb0d172e209228e03ffbdc0d92211a703
mahmoudbalbaa <ma7moudbalbaa@gmail.com>
2024-06-04 15:00:07 -0700

    tsk 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const list = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+
+  return (list);
+}
