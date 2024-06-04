eb2f368abf505c38aa8662fddd333b68f1e19996
mahmoudbalbaa <ma7moudbalbaa@gmail.com>
2024-06-04 15:06:56 -0700

    tsk 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  const list = [
-    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-  return (list);
+  return students;
 }
