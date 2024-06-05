060c0aa85f7401871d4300d3d4a2fc4973176f66
mahmoudbalbaa <ma7moudbalbaa@gmail.com>
2024-06-04 17:15:45 -0700

    tsk 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
-  const sum = students.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const sum = students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
   return sum;
 }
